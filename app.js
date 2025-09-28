bd2b2d8c83d635be2ca08454af0e1f954bf7104d
sanchezarnol17 <sanchezarnol17@gmail.com>
2025-09-28 00:19:08 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+// Aquí va todo el JavaScript que tenías en tu <script> grande del index.
+// Lo movimos a un archivo separado para mantener el proyecto limpio.
+console.log("App.js cargado correctamente");
